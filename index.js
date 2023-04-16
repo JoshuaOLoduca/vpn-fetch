2e26e5017e7498898352270d41420759c836f995
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2023-04-15 23:01:53 -0400

    fix: spacing and semicolons usage in main file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { spawn, exec } from 'node:child_process';
-import got from 'got';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import chalk from 'chalk';
-export class VpnGot {
+import { spawn, exec } from "node:child_process";
+import got from "got";
+import path from "path";
+import { fileURLToPath } from "url";
+import chalk from "chalk";
+export class VPNFetch {
     configFile;
     loginFile;
     tableId;
@@ -25,14 +25,14 @@ export class VpnGot {
       grep -e "[0-9]"`;
             exec(command, (err, stdout) => {
                 if (err) {
-                    console.log('err', err);
-                    resolve('10');
+                    console.log("err", err);
+                    resolve("10");
                 }
-                console.log('stdout', stdout);
+                console.log("stdout", stdout);
                 const existingIds = stdout
                     .toString()
                     .trim()
-                    .split('\n')
+                    .split("\n")
                     .sort((a, b) => Number(a) - Number(b));
                 const nextId = Number(existingIds[existingIds.length - 1]) + 10;
                 resolve(nextId.toString());
@@ -45,10 +45,10 @@ export class VpnGot {
         return new Promise(async (resolve, reject) => {
             this.tableId = await this.getNewTableId();
             const startCommand = [
-                'openvpn',
-                '--script-security',
-                '2',
-                '--route-noexec',
+                "openvpn",
+                "--script-security",
+                "2",
+                "--route-noexec",
                 `--route-up`,
                 `'${__dirname}/route_up.sh`,
                 `${this.tableId}'`,
@@ -57,41 +57,41 @@ export class VpnGot {
                 `--auth-user-pass`,
                 `${this.loginFile}`,
             ];
-            this.pkillFind = startCommand.join(' ').replace(/\'/g, '');
-            const ovpnClient = spawn('sudo', startCommand, {
+            this.pkillFind = startCommand.join(" ").replace(/\'/g, "");
+            const ovpnClient = spawn("sudo", startCommand, {
                 env: { TABLE_ID: this.tableId.toString() },
                 shell: true,
             });
-            ovpnClient.stdout.on('data', (chunk) => {
+            ovpnClient.stdout.on("data", (chunk) => {
                 chunk
                     .toString()
-                    .split('\n')
+                    .split("\n")
                     .forEach((line) => {
                     const data = line.trim();
-                    console.log('data', data);
-                    if (data.toString().includes('[[network interface]]')) {
-                        this.interface = data.toString().split(':')[1]?.trim();
-                        console.log('data.toString()', data.toString());
+                    console.log("data", data);
+                    if (data.toString().includes("[[network interface]]")) {
+                        this.interface = data.toString().split(":")[1]?.trim();
+                        console.log("data.toString()", data.toString());
                         console.log(chalk.green(`Sucessfully created VPN interface on ${this.interface}`));
                     }
-                    if (data.toString().includes('AUTH_FAILED')) {
+                    if (data.toString().includes("AUTH_FAILED")) {
                         reject(new Error(`Auth failed for ${this.configFile}`));
                     }
-                    if (data.toString().includes('Initialization Sequence Completed')) {
+                    if (data.toString().includes("Initialization Sequence Completed")) {
                         console.log(chalk.green(data.toString().trim()));
                         this.ovpnProcess = ovpnClient;
                         resolve(this);
                     }
                 });
             });
-            ovpnClient.stderr.on('data', (data) => {
+            ovpnClient.stderr.on("data", (data) => {
                 console.error(`stderr: ${data}`);
             });
-            ovpnClient.on('error', (err) => {
-                console.error('Error spawning ovpn:', err);
+            ovpnClient.on("error", (err) => {
+                console.error("Error spawning ovpn:", err);
             });
-            ovpnClient.on('close', (code) => {
-                console.log('openvpn exited with code', code);
+            ovpnClient.on("close", (code) => {
+                console.log("openvpn exited with code", code);
             });
         });
     }
@@ -113,7 +113,7 @@ export class VpnGot {
         else {
             opts.localAddress = this.interface;
         }
-        console.log('opts', opts);
+        console.log("opts", opts);
         return await got.get(url, opts);
     }
 }
